9fd1e79d0dd7e450f3fe45af5e4347f88d71c6b6
Antoine Amy <41865290+antoine-amy@users.noreply.github.com>
2022-01-27 07:59:30 +0100

    Add files via upload

---
+++ hello_world.c
@@ -0,0 +1,8 @@
+//To compile go in the menu Terminal, then click run build task, to then run the executable with the command ./hello_world in this folder
+
+#include <stdio.h>
+int main() {
+   // printf() displays the string inside quotation
+   printf("Hello, World!");
+   return 0;
+}
